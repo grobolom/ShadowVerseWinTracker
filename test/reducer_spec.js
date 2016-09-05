1fd8373f148ee8ea6eb303e93132637d5dd6e000
Vasja Volin <vasja.volin@gmail.com>
2016-09-05 15:50:16 -0400

    fix bad naming and missing include in test

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -1,7 +1,9 @@
 import {List, Map, fromJS} from 'immutable';
 import {expect} from 'chai';
 
-describe('reducers', () => {
+import {reducer} from '../tracker/reducer.js';
+
+describe('reducer', () => {
   it('should select a hero', () => {
     const initialState = Map();
     const action = { name: 'SELECT_HERO', leader: 'SWORDCRAFT' };
