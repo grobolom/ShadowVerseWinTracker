139371ca2b254d6f638f9d4bf383e7a123488a37
Vasja Volin <vasja.volin@gmail.com>
2016-09-06 15:12:44 -0400

    switch export default to export because of wonky traspiling

--- tracker/reducer.js
+++ tracker/reducer.js
@@ -1,4 +1,4 @@
-export default function reducer(state, action) {
+export function reducer(state, action) {
   switch(action.type) {
     case 'SELECT_HERO':
       return selectHero(state, action.leader);
