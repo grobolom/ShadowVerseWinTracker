60528aaf3550c1a87ee68825fc56a08a0ed47a97
Vasja Volin <vasja.volin@gmail.com>
2016-09-06 16:21:18 -0400

    add a simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: [
+    './tracker/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
