fcf71b4e57200ab3f77e58f834b37f3f484cef2f
Vasja Volin <vasja.volin@gmail.com>
2016-09-08 17:43:01 -0400

    consolidate simple code directly into jsx for ResultPicker

--- src/components/ResultPicker.jsx
+++ src/components/ResultPicker.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 export default React.createClass({
   getButtons: function() { return ['Won','Lost','D/C']; },
-  getSelected: function() { return this.props.selected; },
-  isSelected: function(name, selected) {
-    return (name === selected) ? "selected" : '';
-  },
   render: function() {
     return <div className="result-picker">
       {this.getButtons().map(name =>
         <button
-          className={this.isSelected(name, this.getSelected())}
-          key={name} >
+          className={name == this.props.selected ? 'selected' : ''}
+          key={name}
+          onClick={() => { this.props.setResult(name); }}
+          >
           {name}
         </button>
       )}
