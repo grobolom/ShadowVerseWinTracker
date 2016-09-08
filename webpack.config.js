f8a60abbf0e4939b0dba060c8830855a1b8f95c2
Vasja Volin <vasja.volin@gmail.com>
2016-09-08 14:04:52 -0400

    add webpack hot reloader config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.css$/,
-      loader: 'style!css!autoprefixer?browsers=last 2 versions'
+      loader: 'react-hot!babel'
     }]
   },
   resolve: {
@@ -21,6 +22,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
