c4b131247b09de3b0d41ad4fecddd621b7fd8f05
Vasja Volin <vasja.volin@gmail.com>
2016-09-08 17:24:24 -0400

    change result picker into something resembling the real component

--- src/components/ResultPicker.jsx
+++ src/components/ResultPicker.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 export default React.createClass({
-  getPair: function() {
-    return this.props.pair || [];
+  getSelected: function() {
+    return this.props.selected;
   },
   render: function() {
-    return <div className="voting">
-      {this.getPair().map(entry =>
-        <button key={entry}>
-          <h1>{entry}</h1>
-        </button>
-      )}
+    return <div className="result-picker">
+        <button>Won</button>
+        <button>Lost</button>
+        <button>D/C</button>
     </div>;
   }
 });
