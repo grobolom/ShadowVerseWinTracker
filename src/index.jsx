4137755a52e62c64465400e7530d199c498446e6
Vasja Volin <vasja.volin@gmail.com>
2016-09-08 14:13:18 -0400

    add a sample top-level component call to our index file

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,11 @@
-console.log('I am alive!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Tracker';
+
+const hero = 'SHADOWCRAFT';
+const villain = 'SWORDCRAFT';
+
+ReactDOM.render(
+  <Tracker hero={hero} villain={villain} />,
+  document.getElementById('app')
+);
