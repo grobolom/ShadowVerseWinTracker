430ede263e3c91c0a4d85af9a5fb33e3016f5c74
Vasja Volin <vasja.volin@gmail.com>
2016-09-09 13:49:31 -0400

    add a top level component for holding our react components

---
+++ src/components/WinTracker.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ResultPicker from './ResultPicker';
+
+export default React.createClass({
+  render: () => {
+    return <div class='container'>
+      <ResultPicker />
+    </div>
+  }
+});
