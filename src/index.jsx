336b5e1dd106c1d412065638adc07d8fc4e2b01a
Vasja Volin <vasja.volin@gmail.com>
2016-09-09 13:53:23 -0400

    fix weird module hotloading issue

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WinTracker from './components/WinTracker';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const selected = 'WON';
 
 ReactDOM.render(
