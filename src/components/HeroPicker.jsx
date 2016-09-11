0d0bcdc67f3cc240df95c6deadce74bd0686cc3f
Vasja Volin <vasja.volin@gmail.com>
2016-09-11 19:58:21 -0400

    add a hero picker component

---
+++ src/components/HeroPicker.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default React.createClass({
+  getLeaders: function() {
+    return [
+      'Forestcraft',
+      'Swordcraft',
+      'Runecraft',
+      'Dragoncraft',
+      'Shadowcraft',
+      'Bloodcraft',
+      'Havencraft',
+    ]
+  },
+  render: function() {
+    return <div>
+      <h1>{this.props.header}</h1>
+      {this.getLeaders().map(name =>
+        <button className='leader'>{name}</button>
+      )}
+    </div>
+  }
+});
--- src/components/WinTracker.jsx
+++ src/components/WinTracker.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ResultPicker from './ResultPicker';
+import HeroPicker from './HeroPicker';
 
 export default React.createClass({
   render: function() {
     return <div className='container'>
+      <HeroPicker header={'Hero'}/>
+      <HeroPicker header={'Villain'}/>
       <ResultPicker
         setResult={this.props.setResult}
       />
