9d1e7befb9a01f2ea822cca4579b51bd0eb6be1d
Vasja Volin <vasja.volin@gmail.com>
2016-09-13 16:57:17 -0400

    add some styling to get the app to a nice state

--- src/components/HeroPicker.jsx
+++ src/components/HeroPicker.jsx
@@ -6,29 +6,29 @@ import * as actionCreators from '../action_creators';
 export const HeroPicker = React.createClass({
   getLeaders: function() {
     return [
-      'Forestcraft',
-      'Swordcraft',
-      'Runecraft',
-      'Dragoncraft',
-      'Shadowcraft',
-      'Bloodcraft',
-      'Havencraft',
+      'forestcraft',
+      'swordcraft',
+      'runecraft',
+      'dragoncraft',
+      'shadowcraft',
+      'bloodcraft',
+      'havencraft',
     ]
   },
   getSelected: function(player, name) {
     if (player === 'Hero' && name == this.props.hero) {
-      return 'button-primary selected';
+      return 'button-primary selected ' + name;
     }
 
     if (player === 'Villain' && name == this.props.villain) {
-      return 'button-primary selected';
+      return 'button-primary selected ' + name;
     }
 
-    return '';
+    return name;
   },
   render: function() {
-    return <div>
-      <h1>{this.props.player}</h1>
+    return <div className='section row'>
+      <h2>{this.props.player}</h2>
       {this.getLeaders().map(name =>
         <button
           className={'leader ' + this.getSelected(this.props.player, name)}
