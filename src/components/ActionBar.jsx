14086c8d6bab5845531ad66d051cff0ddb53d978
Vasja Volin <vasja.volin@gmail.com>
2016-09-15 15:23:28 -0400

    add a dumb component for the action bar

---
+++ src/components/ActionBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import * as actionCreators from '../action_creators';
+
+export const ActionBar = React.createClass({
+  render: function() {
+    return <div className='section row'>
+      <button className='button'>save</button>
+      <button className='button'>stats</button>
+    </div>
+  }
+});
+
+const mapStateToProps = function(state) {
+  return {
+    hero: state.get('hero'),
+    villain: state.get('villain'),
+    result: state.get('result'),
+  };
+};
+
+export const ActionBarComponent = connect(
+  mapStateToProps,
+  actionCreators
+)(ActionBar);
