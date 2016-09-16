818797b2c755fcbe4c5d6b1e0831f54796aa2272
Vasja Volin <vasja.volin@gmail.com>
2016-09-16 16:38:10 -0400

    add a simple win-loss-dc result string in the stat display

--- src/components/StatDisplay.jsx
+++ src/components/StatDisplay.jsx
@@ -9,11 +9,18 @@ export const StatDisplay = React.createClass({
   getResults: function() {
     return this.props.games ? reduceResults(this.props.games) : Map({});
   },
+  getResultString: function(result) {
+    return result.get('wins', '0')
+      + '-'
+      + result.get('losses', '0')
+      + '-'
+      + result.get('d/cs', '0');
+  },
   render: function() {
     return <div className="something">
       <ul>
       {this.getResults().entrySeq().map(result =>
-        <li key={ result[0] }>{ result[0] } - { result[1] }</li>
+        <li key={ result[0] }>{ result[0] } : { this.getResultString(result[1]) }</li>
       )}
       </ul>
     </div>;
