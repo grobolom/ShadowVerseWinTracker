9a0e61ee11ba222d2c870cafe5943829f6598b61
Vasja Volin <vasja.volin@gmail.com>
2016-09-16 15:57:14 -0400

    add results reducer wiring to the Stat Display Component

--- src/components/StatDisplay.jsx
+++ src/components/StatDisplay.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 import * as actionCreators from '../action_creators';
+import {reduceResults} from '../core';
 
 export const StatDisplay = React.createClass({
   getResults: function() {
-    return this.props.games ? this.props.games.reduce((reduction, current) => {
-      const key = current.get('hero') + '-' current.get('villain');
-      const result = current.get('result');
-      return reduction.update(key, 0, wins => wins += (result === 'won' ? 1 : 0));
-    }, List()) : '';
+    return this.props.games ? reduceResults(this.props.games) : Map({});
   },
   render: function() {
     return <div className="something">
       <ul>
-      {this.getResults().map(result =>
-        <li>{ result }</li>
+      {this.getResults().entrySeq().map((key, result) =>
+        <li key={key}>{key} - { result }</li>
       )}
       </ul>
     </div>;
