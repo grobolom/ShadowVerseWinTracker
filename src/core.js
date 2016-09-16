114d3df0dd88301c181e6a2ead5a5ea9c7861304
Vasja Volin <vasja.volin@gmail.com>
2016-09-16 16:38:00 -0400

    switch to a case statement for clarity

--- src/core.js
+++ src/core.js
@@ -5,9 +5,11 @@ export function reduceResults(games) {
     const key = current.get('hero') + ' - ' + current.get('villain');
     const result = current.get('result');
     return reduction.update(key, Map(), pair => {
-      if (result === 'won') return pair.update('wins', 0, g => g += 1);
-      if (result === 'lost') return pair.update('losses', 0, g => g += 1);
-      if (result === 'd/c') return pair.update('d/cs', 0, g => g += 1);
+      switch (result) {
+        case 'won': return pair.update('wins', 0, g => g += 1);
+        case 'lost': return pair.update('losses', 0, g => g += 1);
+        case 'd/c': return pair.update('d/cs', 0, g => g += 1);
+      };
     });
   }, Map());
 };
