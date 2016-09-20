15ca5127a02af915989486d0aeb09aee1e313639
Vasja Volin <vasja.volin@gmail.com>
2016-09-19 20:03:08 -0400

    change WinGrid to temporarily generate random data for all leaders

--- src/components/WinGrid.jsx
+++ src/components/WinGrid.jsx
@@ -1,33 +1,23 @@
 import React from 'react';
-import {List, Map} from 'immutable';
+import {List, Map, Range} from 'immutable';
 
-const colors = {
-  '90': '#009c1a',
-  '75': '#22b600',
-  '50': '#26cc00',
-  '25': '#7be382',
-  '10': '#d2f2d4',
-  'na': '#000000',
+import {getRecordColor} from '../core';
+
+const getRandomRecord = () => {
+  const wins = Math.floor(Math.random() * 10)
+  const losses = Math.floor(Math.random() * 10)
+  return [wins, losses];
 }
+
 const leaders = [ 'fo', 'sw', 'ru', 'dr', 'sh', 'bl', 'ha', ];
-const data = [
-  { 'x': '0', 'y': '0', 'percentage': '25', 'record': '1-3-0'},
-  { 'x': '1', 'y': '0', 'percentage': '10', 'record': '1-9-0'},
-  { 'x': '2', 'y': '0', 'percentage': '25', 'record': '1-3-0'},
-  { 'x': '3', 'y': '0', 'percentage': '75', 'record': '3-1-0'},
-  { 'x': '0', 'y': '1', 'percentage': '90', 'record': '9-0-3'},
-  { 'x': '1', 'y': '1', 'percentage': '50', 'record': '3-3-2'},
-  { 'x': '2', 'y': '1', 'percentage': '75', 'record': '4-1-0'},
-  { 'x': '3', 'y': '1', 'percentage': '90', 'record': '5-1-2'},
-  { 'x': '0', 'y': '2', 'percentage': '10', 'record': '3-1-0'},
-  { 'x': '1', 'y': '2', 'percentage': '10', 'record': '1-7-0'},
-  { 'x': '2', 'y': '2', 'percentage': '50', 'record': '3-4-2'},
-  { 'x': '4', 'y': '2', 'percentage': '50', 'record': '7-8-0'},
-  { 'x': '0', 'y': '3', 'percentage': '90', 'record': '6-1-0'},
-  { 'x': '1', 'y': '3', 'percentage': '10', 'record': '2-1-1'},
-  { 'x': '2', 'y': '5', 'percentage': '75', 'record': '6-2-0'},
-  { 'x': '6', 'y': '2', 'percentage': '75', 'record': '7-3-3'},
-];
+
+const data = Range(0, 7).map( i => {
+  return Range(0, 7).map( j => {
+    return { 'x': i, 'y': j, 'record': getRandomRecord() };
+  });
+}).flatten().toJS();
+
+console.log(data);
 
 export const WinGrid = React.createClass({
   render: function() {
@@ -58,7 +48,7 @@ export const WinGrid = React.createClass({
               height='50px'
               x={ d.x * 50 + 50 }
               y={ d.y * 50 + 50 }
-              style={{ fill: colors[d.percentage] }}
+              style={{ fill: getRecordColor(d.record[0], d.record[1]) }}
               ></rect>
             <text
               textAnchor='middle'
@@ -70,8 +60,9 @@ export const WinGrid = React.createClass({
                 fontSize: '1.3rem',
                 fontFamily: 'courier',
               }}
-              >{ d.record }</text>
+              >{ d.record[0] + '-' + d.record[1]}</text>
           </g>
+          )}
         )}
         </g>
       </svg>
