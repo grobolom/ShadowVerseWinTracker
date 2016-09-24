5963ba7114dfe1d3563e81901e60795ef9dafd87
Vasja Volin <vasja.volin@gmail.com>
2016-09-24 15:50:33 -0400

    attempt to debug redux persist immutable rehydration

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {compose, createStore} from 'redux';
-import {persistStore, autoRehydrate} from 'redux-persist-immutable';
+import {getStoredState, persistStore, autoRehydrate, createPresistor} from 'redux-persist-immutable';
 import {Provider} from 'react-redux';
 
 import reducer from './reducer';
@@ -16,6 +16,14 @@ if (module.hot) {
 require('./normalize.css');
 require('./skeleton.css');
 require('./style.css');
+
+const persistConfig = {}
+
+getStoredState(persistConfig, (err, restoredState) => {
+  console.log('bacon')
+  const store = createStore(reducer, restoredState)
+  const persistor = createPersistor(store, persistConfig)
+})
 
 const store = createStore(reducer, undefined, autoRehydrate());
 
